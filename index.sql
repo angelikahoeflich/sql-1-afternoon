65d863dbc852a441e0fb952610da7ae9e4fa90d7
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-03 14:11:05 -0800

    database problem 3

--- index.sql
+++ index.sql
@@ -111,17 +111,25 @@
 
     EMPLOYEE
 
-    
+    SELECT first_name , last_name FROM employee
+    WHERE city = 'Calgary';
+
+    SELECT MAX(birth_date) FROM employee;
+
+    SELECT MIN(birth_date) FROM employee;
 
 
 
+    SELECT * FROM employee
+    WHERE first_name ='Nancy';
 
+    id shown to be 2
 
+    SELECT * FROM employee
+    WHERE reports_to = 2;
 
-
-
-
-
+    SELECT count(*) FROM employee
+    WHERE city = 'Lethbridge';
 
 
   INVOICE
